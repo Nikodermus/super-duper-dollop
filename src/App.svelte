2e39c791a0c0a709782bde5cde4ad4c0fc364203
Nicolas M. Pardo <nicolasm@nikodermus.media>
2021-03-12 21:18:53 -0500

    App chiquitita

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,35 @@
-<h1>I'm ready!</h1>
+<script>
+    import Letter from './Letter.svelte';
+
+    let value = '';
+    let active = false;
+
+    $: {
+        if (value) {
+            active = false;
+        }
+    }
+</script>
+
+<main>
+    <input type="text" bind:value />
+    <button on:click={() => (active = true)}>Run</button>
+
+    {#if active}
+        <div>
+            {#each value as letter, i}
+                <Letter count={i + 1}>{letter}</Letter>
+            {/each}
+        </div>
+    {/if}
+</main>
+
+<style>
+    main {
+        border: 2px solid #454545;
+        padding: 2rem;
+        border-radius: 20px;
+        width: 50vw;
+        margin: 20vh auto;
+    }
+</style>
---
+++ src/Letter.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let count = 1;
+    export let active = false;
+
+    onMount(() => {
+        setTimeout(() => {
+            active = true;
+        }, count * 200);
+    });
+</script>
+
+<span class:active>
+    <slot />
+</span>
+
+<style>
+    @keyframes appear {
+        80% {
+            color: rebeccapurple;
+        }
+
+        to {
+            transform: scale(1) rotate(0deg);
+            color: hotpink;
+        }
+    }
+
+    span {
+        transform: scale(0) rotate(30deg);
+        color: crimson;
+        text-transform: uppercase;
+        font-size: 40px;
+        display: inline-block;
+    }
+
+    .active {
+        animation: appear 0.3s ease-in-out forwards;
+    }
+</style>
